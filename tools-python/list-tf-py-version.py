4237dd996660bbb8c40152c22888dfb6a43fa2a8
Aaron Zhang <aaronzhang0@gmail.com>
2019-09-24 15:48:45 +0900

    Create list-tf-py-version.py

---
+++ tools-python/list-tf-py-version.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import sys
+
+print("python version is : ")
+print(sys.version)
+print("tensorflow version is : ")
+print(tf.__version__)
+print("tensorflow keras version is : ")
+print(tf.keras.__version__)
+
+
